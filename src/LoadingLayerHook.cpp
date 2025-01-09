27ece12818cc807f56685bffedd315d2e724a1fd
Tiago1257 <84469804+Tiago1257@users.noreply.github.com>
2025-01-09 15:29:18 -0300

    Update LoadingLayerHook.cpp

--- src/LoadingLayerHook.cpp
+++ src/LoadingLayerHook.cpp
@@ -10,7 +10,7 @@ class $modify(APLoadingLayer, LoadingLayer) {
         if (!LoadingLayer::init(p0)) return false;
 
         auto apLabel = CCLabelBMFont::create("ArchipelagoGD: Connecting to archipelago.gg:12345", "goldFont.fnt");
-        apLabel->setPosition(CCDirector::get()->getWinSize().width / 2, 25.f);
+        apLabel->setPosition(CCDirector::get()->getWinSize().width / 2, CCDirector::get()->getWinSize().height - 10);
         apLabel->setScale(.45f);
         apLabel->setID("ap-status-label"_spr);
         this->addChild(apLabel);
